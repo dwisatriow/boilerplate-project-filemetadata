877e1dd89789e6723c7aa451a8db2883355cb77e
dwisatriow <dwisatrioow@gmail.com>
2021-11-14 23:33:34 +0700

    Setup body parser and request logger

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+// Setup server
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require("body-parser");
 
 var app = express();
 
+// Setup cors and body parser
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Request logger
+app.use(function (req, res, next) {
+  var log = req.method + " " + req.path + " - " + req.ip;
+  console.log(log);
+  next();
+});
+
+// Serve public as static
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// Serve index.html
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -14,6 +29,7 @@ app.get('/', function (req, res) {
 
 
 
+// Port listener
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
